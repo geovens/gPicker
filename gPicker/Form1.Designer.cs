7d8b10c2f6ba73e18217874c8e0fcec472101201
geovens <hi@geovens.name>
2015-05-10 21:12:16 +0800

    init commit

---
+++ gPicker/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace WindowsFormsApplication1
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.lbM = new System.Windows.Forms.Label();
+			this.tbInput = new System.Windows.Forms.TextBox();
+			this.tiLayout = new System.Windows.Forms.Timer(this.components);
+			this.tiFade = new System.Windows.Forms.Timer(this.components);
+			this.tiWait = new System.Windows.Forms.Timer(this.components);
+			this.btPick = new WindowsFormsApplication1.MyButton();
+			this.btAdd = new WindowsFormsApplication1.MyButton();
+			this.SuspendLayout();
+			// 
+			// lbM
+			// 
+			this.lbM.AutoSize = true;
+			this.lbM.Font = new System.Drawing.Font("Microsoft YaHei", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+			this.lbM.Location = new System.Drawing.Point(30, 22);
+			this.lbM.Name = "lbM";
+			this.lbM.Size = new System.Drawing.Size(73, 28);
+			this.lbM.TabIndex = 1;
+			this.lbM.Text = "label1";
+			// 
+			// tbInput
+			// 
+			this.tbInput.BackColor = System.Drawing.SystemColors.Window;
+			this.tbInput.BorderStyle = System.Windows.Forms.BorderStyle.None;
+			this.tbInput.Font = new System.Drawing.Font("Microsoft YaHei", 15.75F);
+			this.tbInput.ImeMode = System.Windows.Forms.ImeMode.HangulFull;
+			this.tbInput.Location = new System.Drawing.Point(35, 64);
+			this.tbInput.Margin = new System.Windows.Forms.Padding(0);
+			this.tbInput.MaximumSize = new System.Drawing.Size(1000, 25);
+			this.tbInput.Name = "tbInput";
+			this.tbInput.Size = new System.Drawing.Size(432, 28);
+			this.tbInput.TabIndex = 2;
+			this.tbInput.Visible = false;
+			this.tbInput.TextChanged += new System.EventHandler(this.tbInput_TextChanged);
+			this.tbInput.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tbInput_KeyPress);
+			// 
+			// tiLayout
+			// 
+			this.tiLayout.Enabled = true;
+			this.tiLayout.Interval = 30;
+			this.tiLayout.Tick += new System.EventHandler(this.tiLayout_Tick);
+			// 
+			// tiFade
+			// 
+			this.tiFade.Interval = 30;
+			this.tiFade.Tick += new System.EventHandler(this.tiFade_Tick);
+			// 
+			// tiWait
+			// 
+			this.tiWait.Interval = 400;
+			this.tiWait.Tick += new System.EventHandler(this.tiWait_Tick);
+			// 
+			// btPick
+			// 
+			this.btPick.FlatAppearance.BorderSize = 0;
+			this.btPick.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Gainsboro;
+			this.btPick.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(230)))), ((int)(((byte)(230)))));
+			this.btPick.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.btPick.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+			this.btPick.Image = global::Properties.Resources.pick;
+			this.btPick.Location = new System.Drawing.Point(66, 123);
+			this.btPick.Name = "btPick";
+			this.btPick.Size = new System.Drawing.Size(21, 21);
+			this.btPick.TabIndex = 0;
+			this.btPick.TabStop = false;
+			this.btPick.UseVisualStyleBackColor = true;
+			this.btPick.Click += new System.EventHandler(this.btPick_Click);
+			// 
+			// btAdd
+			// 
+			this.btAdd.FlatAppearance.BorderSize = 0;
+			this.btAdd.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Gainsboro;
+			this.btAdd.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(230)))), ((int)(((byte)(230)))));
+			this.btAdd.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.btAdd.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+			this.btAdd.Image = ((System.Drawing.Image)(resources.GetObject("btAdd.Image")));
+			this.btAdd.Location = new System.Drawing.Point(32, 123);
+			this.btAdd.Name = "btAdd";
+			this.btAdd.Size = new System.Drawing.Size(21, 21);
+			this.btAdd.TabIndex = 3;
+			this.btAdd.TabStop = false;
+			this.btAdd.UseVisualStyleBackColor = true;
+			this.btAdd.Click += new System.EventHandler(this.btAdd_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(520, 186);
+			this.Controls.Add(this.tbInput);
+			this.Controls.Add(this.btPick);
+			this.Controls.Add(this.btAdd);
+			this.Controls.Add(this.lbM);
+			this.MaximizeBox = false;
+			this.Name = "Form1";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "gPicker";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private MyButton btPick;
+		private System.Windows.Forms.Label lbM;
+		private System.Windows.Forms.TextBox tbInput;
+		private MyButton btAdd;
+		private System.Windows.Forms.Timer tiLayout;
+		private System.Windows.Forms.Timer tiFade;
+		private System.Windows.Forms.Timer tiWait;
+	}
+}
+
---
+++ gPicker/Form1.cs
@@ -0,0 +1,330 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Threading;
+using System.Text;
+using System.Windows.Forms;
+using Demot.RandomOrgApi;
+
+namespace WindowsFormsApplication1
+{
+	public partial class Form1 : Form
+	{
+		RandomOrgApiClient ROClient;
+		List<string> Items;
+		List<Button> DeleteButtons;
+		bool Inputing;
+		int Fading;
+		bool InputChanged;
+		string Result;
+
+		int DestHeight;
+		int DestWidth;
+		int DestPickTop;
+
+		const int LineSpace = 28;
+		const int BaseButtonTop = 20;
+		const int BaseHeight = 95;
+		const int MinWidth = 135;
+		const int BaseWidth = 70;
+
+		public Form1()
+		{
+			InitializeComponent();
+			Items = new List<string>();
+			DeleteButtons = new List<Button>();
+			Fading = 4;
+			InputChanged = false;
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			ROClient = new RandomOrgApiClient("627cc29b-fabd-4f00-b0b0-2fead2262323");
+
+			lbM.Text = "";
+			lbM.Top = 10;
+			btAdd.Top = BaseButtonTop;
+			btPick.Top = BaseButtonTop;
+			this.Height = BaseHeight;
+			this.Width = MinWidth;
+		}
+
+		private void btAdd_Click(object sender, EventArgs e)
+		{
+			if (Fading < 4)
+				return;
+
+			if (!Inputing)
+			{
+				tbInput.Text = "";
+				tbInput.Visible = true;
+				tbInput.Focus();
+				Inputing = true;
+			}
+			else
+			{
+				Add(tbInput.Text);
+				Inputing = false;
+			}
+		}
+
+		private void btPick_Click(object sender, EventArgs e)
+		{
+			if (Fading < 4)
+				return;
+			if (Items.Count < 2)
+				return;
+			if (Inputing)
+				Inputing = false;
+
+			foreach (Button button in DeleteButtons)
+				button.Visible = false;
+
+			waiticon = 0;
+			tiWait.Enabled = true;
+			btPick.Image = global::Properties.Resources.wait1;
+
+			Fading = 0;
+			tiFade.Enabled = true;
+		}
+
+
+		private void btDelete_Click(object sender, EventArgs e)
+		{
+			Button me = (Button)sender;
+			for (int i = 0; i < Items.Count; i++)
+			{
+				if (me == DeleteButtons[i])
+				{
+					Delete(i);
+					break;
+				}
+			}
+		}
+
+		private void tbInput_KeyPress(object sender, KeyPressEventArgs e)
+		{
+			if (e.KeyChar == 13)
+			{
+				btAdd_Click(null, null);
+			}
+		}
+
+		private void Add(string str)
+		{
+			Items.Add(str);
+			MyButton aButton = new MyButton();
+			aButton.Width = 11;
+			aButton.Height = 11;
+			aButton.Top = (Items.Count + 1) * LineSpace + 19;
+			aButton.FlatStyle = FlatStyle.Flat;
+			aButton.Left = 15;
+			//aButton.Left = (TextRenderer.MeasureText(str, lbM.Font)).Width + 30;
+			aButton.Click += btDelete_Click;
+			aButton.FlatAppearance.BorderSize = 0;
+			aButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Gainsboro;
+			aButton.Image = global::Properties.Resources.cross_10;
+			this.Controls.Add(aButton);
+			aButton.BringToFront();
+			DeleteButtons.Add(aButton);
+		}
+
+		private void Delete(int i)
+		{
+			Button me = DeleteButtons[i];
+			Items.RemoveAt(i);
+			DeleteButtons.RemoveAt(i);
+			this.Controls.Remove(me);
+			me.Dispose();
+		}
+
+		private void tiLayout_Tick(object sender, EventArgs e)
+		{
+			int N = Items.Count;
+			//lbM.Top = 20;
+
+			string lbm = "";
+			int maxwidth = 0;
+			for (int i = 0; i < N; i++)
+			{
+				lbm += Items[i] + "\r\n";
+				DeleteButtons[i].Top = i * LineSpace + 19;
+				int width = (TextRenderer.MeasureText(Items[i], lbM.Font)).Width;
+				if (width > maxwidth)
+					maxwidth = width;
+			}
+			if (lbM.Text != lbm)
+				lbM.Text = lbm;
+
+			if (Fading < 2)
+			{
+				DestPickTop = BaseButtonTop;
+				DestHeight = BaseHeight;
+				DestWidth = BaseWidth;
+			}
+			else if (Inputing)
+			{
+				tbInput.Top = N * LineSpace + 10;
+				DestPickTop = (N + 1) * LineSpace + BaseButtonTop;
+				DestHeight = (N + 1) * LineSpace + BaseHeight;
+				int inputwidth = (TextRenderer.MeasureText(tbInput.Text, tbInput.Font)).Width;
+				tbInput.Width = Math.Max(inputwidth + 10, 50);
+				if (inputwidth > maxwidth)
+					maxwidth = inputwidth;
+				DestWidth = maxwidth + BaseWidth;
+				if (InputChanged)
+				{
+					int ss = tbInput.SelectionStart;
+					tbInput.SelectionStart = 0;
+					tbInput.SelectionStart = ss;
+					InputChanged = false;
+				}
+			}
+			else
+			{
+				tbInput.Visible = false;
+				DestPickTop = N * LineSpace + BaseButtonTop;
+				DestHeight = N * LineSpace + BaseHeight;
+				DestWidth = maxwidth + BaseWidth;
+			}
+
+			if (DestPickTop != btAdd.Top)
+			{
+				double grow = DestPickTop - btAdd.Top;
+				grow = grow * 0.1;
+				if (Math.Abs(grow) < 1)
+				{
+					grow = Math.Sign(grow);
+				}
+				btAdd.Top += (int)grow;
+				btPick.Top += (int)grow;
+			}
+			if (DestHeight != this.Height)
+			{
+				double grow = DestHeight - this.Height;
+				grow = grow * 0.1;
+				if (Math.Abs(grow) < 1)
+				{
+					grow = Math.Sign(grow);
+				}
+				this.Height += (int)grow;
+			}
+			if (DestWidth < MinWidth)
+				DestWidth = MinWidth;
+			if (DestWidth != this.Width)
+			{
+				double grow = DestWidth - this.Width;
+				grow = grow * 0.1;
+				if (Math.Abs(grow) < 1)
+				{
+					grow = Math.Sign(grow);
+				}
+				this.Width += (int)grow;
+			}
+		}
+
+		int tiFadetick;
+		private void tiFade_Tick(object sender, EventArgs e)
+		{
+			if (Fading == 0)
+			{
+				tiFadetick = 0;
+				Fading = 1;
+			}
+			if (Fading == 1)
+			{
+				tiFadetick++;
+				Color fc = lbM.ForeColor;
+				Color bc = lbM.BackColor;
+				int nr, ng, nb;
+				nr = (int)(fc.R + (bc.R - fc.R) * 0.5);
+				ng = (int)(fc.G + (bc.G - fc.G) * 0.5);
+				nb = (int)(fc.B + (bc.B - fc.B) * 0.5);
+				lbM.ForeColor = Color.FromArgb(nr, ng, nb);
+				if (tiFadetick == 1)
+				{
+					Thread threadget = new Thread(ThreadGet);
+					threadget.Start();
+				}
+				if (tiFadetick >= 50 && Result != null)
+				{
+					while (Items.Count > 0)
+					{
+						Delete(0);
+					}
+					Fading = 2;
+					tiFadetick = 0;
+				}
+			}
+			else if (Fading == 2)
+			{
+				tiFadetick++;
+				if (tiFadetick == 60)
+				{
+					Add(Result);
+					Fading = 3;
+					tiFadetick = 0;
+				}
+			}
+			else if (Fading == 3)
+			{
+				tiFadetick++;
+				Color fc = lbM.ForeColor;
+				Color dc = SystemColors.ControlText;
+				double dr, dg, db;
+				dr = (fc.R - dc.R) * 0.03;
+				dg = (fc.G - dc.G) * 0.03;
+				db = (fc.B - dc.B) * 0.03;
+				if (dr != 0 && Math.Abs(dr) < 1)
+					dr = Math.Sign(dr);
+				if (dg != 0 && Math.Abs(dg) < 1)
+					dg = Math.Sign(dg);
+				if (db != 0 && Math.Abs(db) < 1)
+					db = Math.Sign(db);
+				lbM.ForeColor = Color.FromArgb((int)(fc.R - dr), (int)(fc.G - dg), (int)(fc.B - db));
+				if (tiFadetick == 60)
+				{
+					Fading = 4;
+					tiFadetick = 0;
+					tiFade.Enabled = false;
+				}
+			}
+		}
+
+		private void tbInput_TextChanged(object sender, EventArgs e)
+		{
+			InputChanged = true;
+		}
+
+		int waiticon;
+		private void tiWait_Tick(object sender, EventArgs e)
+		{
+			if (Fading >= 3)
+			{
+				waiticon = 0;
+				this.btPick.Image = global::Properties.Resources.pick;
+				tiWait.Enabled = false;
+				return;
+			}
+			waiticon++;
+			if (waiticon == 3)
+				waiticon = 0;
+			if (waiticon == 0)
+				btPick.Image = global::Properties.Resources.wait1;
+			else if (waiticon == 1)
+				btPick.Image = global::Properties.Resources.wait2;
+			else if (waiticon == 2)
+				btPick.Image = global::Properties.Resources.wait3;
+		}
+
+		private void ThreadGet()
+		{
+			Result = null;
+			Response R = ROClient.GenerateIntegers(1, 0, Items.Count - 1);
+			int ri = R.Integers[0];
+			Result = Items[ri];
+		}
+	}
+}
