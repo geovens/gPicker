28714392ff1d538314161045ea75eda3ccceb9ef
geovens <hi@geovens.name>
2015-05-12 10:42:50 +0800

    add assembly info

--- gPicker/Properties/AssemblyInfo.cs
+++ gPicker/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WindowsFormsApplication1")]
+[assembly: AssemblyTitle("gPicker")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WindowsFormsApplication1")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyProduct("gPicker")]
+[assembly: AssemblyCopyright("© 2015 Geovens")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
